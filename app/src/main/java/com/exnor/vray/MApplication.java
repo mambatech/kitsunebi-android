066ed3215c5d970fd695a4b04512901068f757ad
liaoshouwang <Edison.liao@cootek.cn>
2020-05-23 15:43:14 +0800

    首页加native广告,github

--- app/src/main/java/com/exnor/vray/MApplication.java
+++ app/src/main/java/com/exnor/vray/MApplication.java
@@ -23,7 +23,7 @@ public class MApplication extends Application {
         sIns = this;
         UMConfigure.setLogEnabled(BuildConfig.DEBUG);
         // init umeng sdk
-        UMConfigure.init(this, "5e71efe2167eddc4420000e8", "umeng", UMConfigure.DEVICE_TYPE_PHONE, null);
+        UMConfigure.init(this, "5e71efe2167eddc4420000e8", "github", UMConfigure.DEVICE_TYPE_PHONE, null);
         //统计SDK是否支持采集在子进程中打点的自定义事件，默认不支持
         //UMConfigure.setProcessEvent(true);//支持多进程打点
         MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO);
